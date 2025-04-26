d0d70019c1f98caf66849f37f1b279cb61d30c43
AlainRichell <alainrichell@gmail.com>
2025-04-26 19:28:29 -0400

    Environment update ohboi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    //apiUrl: 'https://alainrichell.pythonanywhere.com',
-    apiUrl: 'http://localhost:8000'
+    apiUrl: 'https://alainrichell.pythonanywhere.com',
+    //apiUrl: 'http://localhost:8000'
   };
