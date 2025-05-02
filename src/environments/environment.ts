3c8cad13d7156c62532bfba85cbc6c29070d8816
Alain Richel Rodríguez Blazquez <alainrichell@gmail.com>
2025-05-02 02:39:30 -0400

    Update environment omg

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: 'https://alainrichell.pythonanywhere.com',
+    apiUrl: 'https://abdulinfo.pythonanywhere.com',
     //apiUrl: 'http://localhost:8000'
   };
